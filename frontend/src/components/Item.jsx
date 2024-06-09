46a8e538ec6098cd86837b9566003c0a788881e9
yuvraj <yuvrajgoswami8182@gmail.com>
2024-06-09 07:10:05 +0530

    version 2.4

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -7,11 +7,12 @@ const Item = ({ id, name, image, old_price, new_price }) => {
     <div className="rounded-xl overflow-hidden shadow-lg">
       <div className="relative flexCenter group overflow-hidden transition-all duration-100">
         <Link
-          to={`product/${id}`}
+          to={`/product/${id}`}
           className="h-12 w-12 bg-white rounded-full flexCenter absolute top-1/2 bottom-1/2 !py-2 z-20 transition-all duration-700">
           <FaSearch className="scale-125 hover:rotate-90  transition-all duration-200" />
         </Link>
         <img
+          onClick={window.scroll(0,0)}
           src={image}
           alt="productImage"
           className="w-full block object-cover group-hover:scale-110 transition-all duration-1000"
--- frontend/src/pages/ProductDescription.jsx
+++ frontend/src/pages/ProductDescription.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const ProductDescription = () => {
   return (
-    <div>ProductDescription</div>
+   <div className='mt-20'>
+       <div className='flex gap-3 mb-4'>
+          <button className='btn_dark_rounded !rounded-none !text-xs !py-[6px] w-36'>Description</button>
+          <button className='btn_dark_outline !rounded-none !text-xs !py-[6px] w-36'>Care Guide</button>
+          <button className='btn_dark_outline !rounded-none !text-xs !py-[6px] w-36'>Size Guide</button>  
+       </div>
+       <div className='flex flex-col pb-16'>
+        <p className='text-sm'>Drive sales with Dynamics 365 texting. TrueDialog helps you convert more leads.
+             Dynamics 365 texting setup too complex? TrueDialog offers a seamless solution.
+             Drive sales with Dynamics 365 texting. TrueDialog helps you convert more leads.
+             Dynamics 365 texting setup too complex? TrueDialog offers a seamless solution.</p>
+             <p className='text-sm'>Drive sales with Dynamics 365 texting. TrueDialog helps you convert more leads.
+             Dynamics 365 texting setup too complex? TrueDialog offers a seamless solution.
+             Drive sales with Dynamics 365 texting. TrueDialog helps you convert more leads.
+             Dynamics 365 texting setup too complex? TrueDialog offers a seamless solution.</p>
+       </div>
+   </div>
   )
 }
 
